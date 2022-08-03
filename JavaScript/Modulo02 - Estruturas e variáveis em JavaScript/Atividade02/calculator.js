51ed8138b0f3f717e8daa3e540ecef6d456d6001
PauloCesar73 <91334082+PauloCesar73@users.noreply.github.com>
2022-08-03 17:51:03 -0300

    Operadores em JS, projeto Calculadora.

--- JavaScript/Modulo02 - Estruturas e variáveis em JavaScript/Atividade02/calculator.js
+++ JavaScript/Modulo02 - Estruturas e variáveis em JavaScript/Atividade02/calculator.js
@@ -4,32 +4,33 @@ var number1, number2, result, operador;
 do {
 
 console.log("Calculadora")
-number1 = prompt("Digite o primeiro número: ")
+number1 = parseFloat(prompt("Digite o primeiro número: "))
 operador = prompt("Digite o sinal da operação desejada(+, -, /, x)")
-number2 = prompt("Digite o segundo número: ")
+number2 = parseFloat(prompt("Digite o segundo número: "))
 
     if (operador == '+') {
         result = number1 + number2
-        console.log(result)
+        console.log("Resultado: ", result)
     }
     else if (operador == '-'){
         result = number1 - number2
-        console.log(result)
+        console.log("Resultado: ", result)
     }
     else if (operador == '/'){
         result = number1 / number2
-        console.log(result)
+        var sobra = number1 % number2
+        console.log("Resultado: ", result, " Sobra: ", sobra)
     }
     else if (operador == 'x' || operador == 'X'){
         result = number1 * number2
-        console.log(result)
+        console.log("Resultado:", result)
     }
     else {
         console.log("Erro!")
     };
 
-    var exit = prompt("Deseja continuar? ")
-if (exit == "Sim" || exit == "sim" || exit == "SIM" || exit == "SiM" || exit == "sIM" || exit == "siM" || exit == "yes" || exit == "Yes" || exit == "YES"){
+    var exit = prompt("Deseja continuar?(Digite sim) ")
+if (exit == "Sim" || exit == "sim" || exit == "SIM" || exit == "SiM" || exit == "sIM" || exit == "siM" || exit == "S" || exit == "s" || exit == "yes" || exit == "Yes" || exit == "YES"){
 exit = true
 }
 else {

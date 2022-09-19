8ffb27ae733d12ac5a4a2bf4a3d43069989b1f26
PauloCesar73 <91334082+PauloCesar73@users.noreply.github.com>
2022-09-19 17:15:27 -0300

    Adapter pattern

---
+++ Padrões de Desenvolvimento de Software/Modulo02/Atividade04/com.adapter.teste/src/com/adapter/teste/Pato.java
@@ -0,0 +1,12 @@
+package com.adapter.teste;
+
+public class Pato {
+	
+		public void quaqua() {
+		System.out.println("QuaQua");
+		}
+		public void voar() {
+		System.out.println("Voou");
+		
+	}
+}

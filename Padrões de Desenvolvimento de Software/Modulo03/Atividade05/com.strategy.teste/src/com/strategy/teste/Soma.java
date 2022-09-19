da8b245b460005163a6b4e7928ac499bb0473bb5
PauloCesar73 <91334082+PauloCesar73@users.noreply.github.com>
2022-09-19 20:16:40 -0300

    Strategy method.

--- Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Soma.java
+++ Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Soma.java
@@ -1,5 +1,9 @@
 package com.strategy.teste;
 
-public class Soma {
-
+public class Soma implements Strategy{
+      public int execute(int n1, int n2) {
+        
+        int resultado = n1+n2;
+        return resultado;
+    }
 }

22269e4a18d76553ed7ed3612e42dcc54ccc6098
PauloCesar73 <91334082+PauloCesar73@users.noreply.github.com>
2022-09-19 20:11:03 -0300

    Strategy method.

--- Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Main.java
+++ Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Main.java
@@ -1,5 +1,36 @@
 package com.strategy.teste;
 
 public class Main {
+  
+  	public static void main(String[] args) {
+        Calcular calculadora = new Calcular();
+        while(true)
+        {
+        	Scanner input = new Scanner(System.in);
+        	System.out.println("Informe o primeiro valor");
+        	int num1 = input.nextInt();
+            System.out.println("Informe o primeiro valor");
+            int num2 = input.nextInt();
+            System.out.println("1 - Somar");
+            System.out.println("2 - Subtrair");
+            System.out.println("3 - Multiplicar");
+            int op = input.nextInt();
+            if(op == 1)
+            {
+                calculadora.calcular = new Soma();
+                calculadora.Operacao(num1, num2);
+            }
+            if (op == 2)
+            {
+                calculadora.calcular = new Subtracao();
+                calculadora.Operacao(num1, num2);
+            }
+            if(op == 3)
+            {
+                calculadora.calcular = new Multiplicacao();
+                calculadora.Operacao(num1, num2);
+                }
+        }
+	}
 
 }

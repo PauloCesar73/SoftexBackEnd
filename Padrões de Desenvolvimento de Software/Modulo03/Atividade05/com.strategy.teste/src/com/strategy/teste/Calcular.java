8e7b77e1d91b014be0be0c383b569fb7a1a98824
PauloCesar73 <91334082+PauloCesar73@users.noreply.github.com>
2022-09-19 20:09:55 -0300

    Strategy method.

--- Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Calcular.java
+++ Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Calcular.java
@@ -1,5 +1,11 @@
 package com.strategy.teste;
 
 public class Calcular {
+  
+  public Strategy calcular;
+	
+	public void Operacao(int n1, int n2) {
+		System.out.println(calcular.execute(n1, n2));
+	}
 
 }

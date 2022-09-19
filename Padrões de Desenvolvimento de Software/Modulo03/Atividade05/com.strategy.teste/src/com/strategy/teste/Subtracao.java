2364bfb49cb0960467be26ea6029229ecea5c083
PauloCesar73 <91334082+PauloCesar73@users.noreply.github.com>
2022-09-19 20:18:48 -0300

    Strategy method

--- Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Subtracao.java
+++ Padrões de Desenvolvimento de Software/Modulo03/Atividade05/com.strategy.teste/src/com/strategy/teste/Subtracao.java
@@ -1,5 +1,9 @@
 package com.strategy.teste;
 
-public class Subtracao {
-
+public class Subtracao implements Strategy{
+      public int execute(int n1, int n2) {
+        
+        int resultado = n1-n2;
+        return resultado;
+    }
 }
